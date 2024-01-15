694525139bdf959916c9864aad9e0cc48036a772
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-01-15 22:24:43 +0100

    router and theme created

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#A0C49D'
+    },
+    secondary: {
+      main: '#C4D7B2'
+    }
+  }
+})
+
+export default theme
