d250f27bb5a8db592b35e7696108ff2e4472df09
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-02-12 23:41:29 +0100

    NavBar input created

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, FormControl, IconButton, Input, InputAdornment, InputLabel } from '@mui/material'
+import { Box, Button, FormControl, IconButton, InputBase, InputAdornment, useMediaQuery } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import Brightness4Icon from '@mui/icons-material/Brightness4'
 import Brightness7Icon from '@mui/icons-material/Brightness7'
@@ -11,10 +11,14 @@ interface NavBarProps {
 }
 
 export const NavBar = (props: NavBarProps) => {
+  const matches = useMediaQuery('(min-width:600px)')
   const { siteMode, onIconClick } = props
   const [isFocused, setIsFocused] = React.useState(false)
   const handleFocus = () => {
     setIsFocused(true)
+  }
+  const handleBlur = () => {
+    setIsFocused(false)
   }
   return (
     <Box
@@ -36,22 +40,31 @@ export const NavBar = (props: NavBarProps) => {
           }}
         />
       </Button>
-      <Box>
-        <FormControl>
-          <InputLabel
-            htmlFor={'input-with-icon-adornment'}
-            sx={{
-              color: (theme) => theme.palette.primary.contrastText
-            }}
-          >
-            Search...
-          </InputLabel>
-          <Input
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <FormControl
+          sx={{
+            marginRight: { xs: '0', sm: '8px' }
+          }}
+        >
+          <InputBase
             id={'input-with-icon-adornment'}
+            size={matches ? 'medium' : 'small'}
             onFocus={handleFocus}
+            onBlur={handleBlur}
+            placeholder={'search...'}
             sx={{
               color: (theme) => theme.palette.primary.contrastText,
-              width: isFocused ? { xs: '12rem', sm: '20rem', md: '30rem' } : '7rem'
+              width: isFocused ? { xs: '12rem', sm: '20rem', md: '30rem' } : '6.2rem',
+              backgroundColor: (theme) => theme.palette.mode === 'dark' ? theme.palette.background.paper : theme.palette.primary.dark,
+              padding: '3px',
+              borderRadius: '5px',
+              boxShadow: 1
             }}
             endAdornment={
               <InputAdornment position={'end'} >
